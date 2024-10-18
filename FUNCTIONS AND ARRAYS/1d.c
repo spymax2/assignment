2f8bf57797ac934a179dab8b45dbc5435a9b7b02
spymax2 <vibertondieki85@gmail.com>
2024-10-18 10:08:07 +0300

    Update 1d.c

--- FUNCTIONS AND ARRAYS/1d.c
+++ FUNCTIONS AND ARRAYS/1d.c
@@ -8,11 +8,12 @@ reg no:CT101/G/22151/24
 #include <stdio.h>
 int main(){
 
-int i,j;
-int marks[2][3]={{1,2,3},{4,5,6}};
-for(i=0;i<2;i++){
-for(j=0;j<3;j++){
-printf("marks [%d][%d]=%d\n",i,j,marks[i][j]);
+int i;
+int marks[3]={{1,2,3};
+  for (i=0;i<3;i++);
+
+
+printf("marks [%d]=%d\n",i,marks[i]);
 }
 }
 
