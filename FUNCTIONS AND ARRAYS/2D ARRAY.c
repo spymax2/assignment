be1c949462c45b7f4d468006368f445848270de5
spymax2 <vibertondieki85@gmail.com>
2024-10-24 14:32:45 +0300

    Add files via upload

---
+++ FUNCTIONS AND ARRAYS/2D ARRAY.c
@@ -0,0 +1,25 @@
+/*
+// program to code a 2d array
+author:vibert
+reg no:CT101/G/22151/24
+
+*/
+#include <stdio.h>
+int main(){
+	int i,j;
+	int marks[2][3]={{1,2,3},{3,4,5}};
+	for(i=0;i<2;i++){
+		for(j=0;j<3;j++){
+		
+		printf("marks[%d][%d]=%d",i,j,marks[i][j]);
+	}
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
